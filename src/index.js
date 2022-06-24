fef56573a7ed00479c91bc7ac49c591fd2feeb4e
NguyenHuuHieuThien <78004234+NguyenHuuHieuThien@users.noreply.github.com>
2022-06-25 03:16:37 +0700

    update

--- src/index.js
+++ src/index.js
@@ -15,12 +15,12 @@ app.engine(
         extname: 'hbs',
     }),
 );
-        app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', 'views');
-    app.set('views', path.join(__dirname, 'resources/views/'));
+app.set('views', path.join(__dirname, 'resources/views/'));
 
 route(app);
- 
-                app.listen(port, () => {
+
+app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
